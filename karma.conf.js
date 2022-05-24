95c41f549ffc2cf66954ffb74e981e9eda1aedb5
Pornthi03 <pornthida8@gmail.com>
2022-05-25 06:15:34 +0700

    UPDATE [ angular.json ]

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/numer'),
+      dir: require('path').join(__dirname, './coverage/numerical'),
       subdir: '.',
       reporters: [
         { type: 'html' },
