9431bef9dc588647ab431f97aaf013d3407c487d
Pornthi03 <pornthida8@gmail.com>
2022-05-27 00:10:12 +0700

    UPDATE[ app module ]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,7 @@ import {MatChipsModule} from '@angular/material/chips';
 import {MatIconModule} from '@angular/material/icon';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {MatDialogModule} from '@angular/material/dialog';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatTableModule} from '@angular/material/table';
@@ -118,7 +118,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
     MatSortModule,
     MatPaginatorModule,
   ],
-  providers: [MatDialogRef],
+  providers: [],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
   bootstrap: [AppComponent]
 })
