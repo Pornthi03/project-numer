3028e5ee1b53893b1fa621ab93c41af25424f60d
Pornthi03 <pornthida8@gmail.com>
2022-05-29 19:08:53 +0700

    UPDATE[ karma ]

--- karma.conf.js
+++ karma.conf.js
@@ -36,8 +36,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
+    singleRun: false
     // ['ChromeHeadlessNoSandbox'],
     // customLaunchers: {
     //   ChromeHeadlessNoSandbox: {
